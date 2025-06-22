b63cf373b894e54180ee884cda810c1f9a4b777a
ravi <gavaderavi4@gmail.com>
2025-06-22 15:46:45 -0400

    vm

--- app/src/main/java/com/csaim/random/EventViewModel.kt
+++ app/src/main/java/com/csaim/random/EventViewModel.kt
@@ -1,19 +1,35 @@
 package com.csaim.random
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.launch
 
 class EventViewModel:ViewModel() {
 
 
-    private val _eventFlow = MutableStateFlow<UiEvent>()
+    //shared flow to emit one time events
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+
+    // trigger the event
+    fun onButtonClicked(){
+        viewModelScope.launch {
+            _eventFlow.emit(UiEvent.ShowToast("supp boi"))
+        }
+    }
 
 
 
 
-
+    // event types
     sealed class UiEvent {
-
+        data class ShowToast(val mesage : String) : UiEvent()
     }
 
 
