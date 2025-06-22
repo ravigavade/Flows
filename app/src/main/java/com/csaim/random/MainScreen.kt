26a3bde18579a1429118466b030d29ef0f565bf3
ravi <gavaderavi4@gmail.com>
2025-06-22 15:57:49 -0400

    ui

---
+++ app/src/main/java/com/csaim/random/MainScreen.kt
@@ -0,0 +1,47 @@
+package com.csaim.random
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun MainScreen(viewModel: EventViewModel = viewModel()) {
+
+    val context = LocalContext.current
+
+    //collect sharedFlow
+    LaunchedEffect(Unit){
+        viewModel.eventFlow.collectLatest { event ->
+            when(event){
+                is EventViewModel.UiEvent.ShowToast -> {
+                    Toast.makeText(context, event.mesage, Toast.LENGTH_LONG)
+                }
+            }
+        }
+    }
+
+    //ui
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                viewModel.onButtonClicked()
+            }
+        ) {
+            Text("Click me")
+        }
+    }
+
+}
